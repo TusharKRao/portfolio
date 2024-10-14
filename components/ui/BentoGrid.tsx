ebdd71105fc98fc90dc76c51d92290abd80f9ecf
Tushar K Rao <tusharkrao999@gmail.com>
2024-10-14 14:22:45 -0500

    work section phase 1 done, moving onto the next one

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -21,6 +21,10 @@ export const BentoGrid = ({
   );
 };
 
+const leftLists = ["ReactJS", "Express", "Typescript"];
+const rightLists = ["Python", "NextJS", "PosgresQL"];
+const rightmostLists = ["Django", "DynamoDB", "Redis"];
+
 export const BentoGridItem = ({
   className,
   title,
@@ -43,7 +47,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 rounded-3xl relative group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
+        "row-span-1 rounded-3xl relative overflow-hidden group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
         className
       )}
       style={{
@@ -74,13 +78,13 @@ export const BentoGridItem = ({
           )} 
         </div>
 
-        {id===6 && (
+        {/* {id===6 && (
           <BackgroundGradientAnimation>
             <div 
-            className="absolute z-50 flex items-center justify-center text-white font-bold"
+            className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"
             />
           </BackgroundGradientAnimation>
-        )}
+        )} */}
 
         <div className={cn(titleClassName, 'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10')} >
           <div className="font-sans font-extra-light text-[#c1c2d3] text-smmd:text-xs lg:text-base z-10">
@@ -89,22 +93,52 @@ export const BentoGridItem = ({
           <div className="font-sans font-bold text-lg lg:text-xl max-w-96 z-10">
             {title}
           </div>
-        
+      
 
-        {/* { id == 3 && <GlobeDemo />} */}
-        { id === 4 && (
-          <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-            <div className="flex flex-col gap-3 lg:gap-8">
-              {['React.js', 'Next.js', 'TypeScript'].map((item) => (
-                <span key={item} className="py-2 lg:py4 lg:px3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
-                  {item}
-                </span>
-              ))}
+        {/* //Tech stack */}
+        </div>
+        {id === 4 && (
+            <div className="flex gap-1 lg:gap-5 w-fit center -ml-20 lg:-right-2">
+              {/* tech stack lists */}
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-5">
+                {leftLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+              </div>
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-5">
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+                {rightLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </div>
+              <div className="flex flex-col gap-3 md:gap-3 lg:gap-5">
+                {rightmostLists.map((item, i) => (
+                  <span
+                    key={i}
+                    className="lg:py-4 lg:px-3 py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                <span className="lg:py-4 lg:px-3 py-4 px-3  rounded-lg text-center bg-[#10132E]"></span>
+              </div>
             </div>
-          </div>
-        )}
+          )}
 
-      </div>
       </div>
     </div>
   );
